23520881a4af5134c29ce776ba48047bca3496a4
Frost <frostedwolf789@gmail.com>
2022-11-19 19:44:29 -0500

    code

---
+++ main.py
@@ -0,0 +1,58 @@
+def readInput():
+    items = {}
+    f = open("input.txt", "r")
+    for x in f:
+        item = x.split(",")
+        items[item[0]]= [float(item[1].strip()), "yuvraj", "meer", "thenu","cynthia","sarah"]
+    f.close()
+    return items
+
+def optOut(items):
+    for name in ["yuvraj", "meer", "thenu","cynthia","sarah"]:
+        f = open(name+".txt","r")
+        for x in f:
+            item = x.strip()
+            items[item].remove(name)
+        f.close()
+    return
+
+def calCost(items):
+    names = {"yuvraj": 0, "meer": 1, "thenu": 2,"cynthia":3,"sarah":4}
+    finalCost=[0]*5
+    for item in items:
+        if (len(items[item]) == 6):
+            for i in range(5):
+                finalCost[i]+=(items[item][0])/5
+        elif (len(items[item]) == 1):
+            print("ERROR: Everyone opted out")
+            break
+        else:
+            for i in range(1,len(items[item])):
+                finalCost[names[items[item][i]]]+=(items[item][0]/(len(items[item])-1))
+
+    return finalCost
+
+def main():
+    count=0
+    total=0
+    items = readInput()
+    optOut(items)
+    calCost
+    finalCost=calCost(items)
+    for i in items:
+        count+=items[i][0]
+    print("Total = {}".format(count))
+    print("Split costs:")
+    print("Yuvraj = {}".format(finalCost[0]))
+    print("Meer = {}".format(finalCost[1]))
+    print("Thenu = {}".format(finalCost[2]))
+    print("Cynthia = {}".format(finalCost[3]))
+    print("Sarah = {}".format(finalCost[4]))
+    for i in finalCost:
+        total+=i
+    print(total)
+
+        
+
+main()
+
